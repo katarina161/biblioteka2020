fe2b8062140f886e7f73a8565127491c5d109fee
katarina161 <katarinanovakovich666@gmail.com>
2020-02-19 14:18:30 +0100

    Napravljena klasa Autor, refaktorisana Knjiga

---
+++ Biblioteka/src/biblioteka/Autor.java
@@ -0,0 +1,30 @@
+package biblioteka;
+
+public class Autor {
+	
+	private String ime;
+	private String prezime;
+	
+	public Autor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+	public void setPrezime(String prezime) {
+		this.prezime = prezime;
+	}
+	
+	
+
+}
